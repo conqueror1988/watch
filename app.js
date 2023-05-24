b50af4b35d1c33c1a4646da409f0bd151b8a4761
Fatih Aksoy <fatih.aksoy1988@gmail.com>
2023-05-24 16:14:56 +0200

    Watch

---
+++ app.js
@@ -0,0 +1,38 @@
+const display = document.querySelector(".ClockDisplay");
+
+function showTime() {
+  let date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+  let session = "AM";
+
+  if (hour === 12 && session == "AM") { //! eger 
+    hour = 0;
+  }
+
+  if (hour === 12 && session == "PM") { //! eger 
+    hour = 12;
+  }
+
+  if (hour > 12) {
+    session = "PM";
+    hour = hour - 12;
+  }
+
+  //!hour >12 ?  session = "PM" : session = "AM"  ikici yol
+
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+  if (second < 10) {
+    second = "0" + second;
+  }
+
+  display.innerHTML = hour + ":" + minute + ":" + second + ":" + session;
+}
+
+setInterval(showTime, 1000);
